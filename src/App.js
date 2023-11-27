7a4655f018e10b7f0499bd08b6b02a24f1567127
David <davidcuerpito@gmail.com>
2023-11-27 17:36:52 -0500

    log variables entorno

--- src/App.js
+++ src/App.js
@@ -4,6 +4,10 @@ import Principal from './componente/Principal';
 //import ListaULM from './componente/SeleccionQuiz';
 
 function App() {
+  useEffect(()=>{
+    console.log(process.env)
+  },[])
+  
   // 2. Crear un useEfect que se encargue de llamar el metodo getQuestionTopics y guarde en localstorag  
   return (
     <Principal /> //3. pasarle por Prop esa informacion cargada, 
