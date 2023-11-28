0a412c3ad8996abf4f5ce8e6f357f3b0700bbae4
David <davidcuerpito@gmail.com>
2023-11-28 09:45:45 -0500

    cambio pdf pequeños

--- src/componente/Pasarela.js
+++ src/componente/Pasarela.js
@@ -3,93 +3,102 @@ import StripeCheckout from 'react-stripe-checkout';
 import axios from 'axios';
 import '../hojas estilo/Pasarela.css';
 import { toast } from 'react-toastify';
-import "react-toastify/dist/ReactToastify.css";
+import 'react-toastify/dist/ReactToastify.css';
 import Cookies from 'universal-cookie';
-import moment from "moment";
+import moment from 'moment';
 const cookies = new Cookies();
 
-
+const STRIPE_PUBLIC_KEY_DEV =
+  'pk_test_51Ns7BdFxlOv4gV2Cr4mU5j5ALd479RPRjBS1MJOc4ixxcoj9VolIlL4Y06N1nUFh1kf09bRpkBXGHM5ZfMvpGtqs00r3YryaYs';
+const STRIPE_PUBLIC_KEY_PROD = '';
 
 const MyCheckoutComponent = () => {
+  const stripePublicKey = process.env.NODE_ENV === 'development' ? STRIPE_PUBLIC_KEY_DEV : STRIPE_PUBLIC_KEY_PROD;
 
-  toast.configure()
+  toast.configure();
 
-    const milisegundosMes = 2592000000;
-    const operacion = "suscripcion";
-    const operacionPago = "pago"; 
-    const [product]= useState({
-    name: "PAGO EXAMEN ULM",
+  const milisegundosMes = 2592000000;
+  const operacion = 'suscripcion';
+  const operacionPago = 'pago';
+  const [product] = useState({
+    name: 'PAGO EXAMEN ULM',
     price: 30,
-    description: "PAGO EXAMEN ULM"
-    })
+    description: 'PAGO EXAMEN ULM'
+  });
 
-    let FechaMes = new Date();
-    var tres = FechaMes.getTime();
-    tres = tres + milisegundosMes;
-    let FechaFechaMesDos = new Date(tres);
-    let Fecha_SuscripcionMes = FechaMes;
-    let Fecha_CaducidadMes = FechaFechaMesDos;
-    Fecha_SuscripcionMes =  moment.utc(Fecha_SuscripcionMes).format('YYYY-MM-DD');
-    Fecha_CaducidadMes =  moment.utc(Fecha_CaducidadMes).format('YYYY-MM-DD');
+  let FechaMes = new Date();
+  var tres = FechaMes.getTime();
+  tres = tres + milisegundosMes;
+  let FechaFechaMesDos = new Date(tres);
+  let Fecha_SuscripcionMes = FechaMes;
+  let Fecha_CaducidadMes = FechaFechaMesDos;
+  Fecha_SuscripcionMes = moment.utc(Fecha_SuscripcionMes).format('YYYY-MM-DD');
+  Fecha_CaducidadMes = moment.utc(Fecha_CaducidadMes).format('YYYY-MM-DD');
 
-  const handleToken = async (token) => {
+  const handleToken = async token => {
     // Envia el token al servidor Node.js
-    const response = await axios.post("https://450dfy2taj.execute-api.us-east-2.amazonaws.com/prod/aeronautica", {
-    //const response = await axios.post("http://localhost:5000/api/charge", {
+    const response = await axios.post('https://450dfy2taj.execute-api.us-east-2.amazonaws.com/prod/aeronautica', {
+      //const response = await axios.post("http://localhost:5000/api/charge", {
       operacionPago: operacionPago,
       token: token.id,
-      amount: 3000, // Cambia esto al monto que quieras cobrar
+      amount: 3000 // Cambia esto al monto que quieras cobrar
     });
 
     // Maneja la respuesta del servidor, por ejemplo, muestra un mensaje de éxito
     console.log(response);
 
-    if ( response.data.message === 'Successful payment') {
-      toast("pago exitoso fue completado",{type:'Exito'})
-      alert(response.data.message)
-      axios.post("https://450dfy2taj.execute-api.us-east-2.amazonaws.com/prod/aeronautica", {
-        operacion: operacion,
-        Id: cookies.get('Id'),
-        Suscripcion: "Activo",
-        Fecha_SuscripcionMes: Fecha_SuscripcionMes,
-        Fecha_CaducidadMes: Fecha_CaducidadMes,
-        }).then((response) => {
-            if(response.data.message){
-              cookies.set('Suscripcion', response.data.content.Suscripcion, {path: "/"});
-              window.location.href="./activa";
-              alert(response.data.message);
-            }else{
-              alert('AL PARECER HAY UN PROBLEMA CON LA ACTUALIZACION DE LA SUSCRIPCION');
-              window.location.href="./noentra";
-            }
+    if (response.data.message === 'Successful payment') {
+      toast('pago exitoso fue completado', { type: 'Exito' });
+      alert(response.data.message);
+      axios
+        .post('https://450dfy2taj.execute-api.us-east-2.amazonaws.com/prod/aeronautica', {
+          operacion: operacion,
+          Id: cookies.get('Id'),
+          Suscripcion: 'Activo',
+          Fecha_SuscripcionMes: Fecha_SuscripcionMes,
+          Fecha_CaducidadMes: Fecha_CaducidadMes
         })
-    }else{
-      toast("pago fallido existe algun problema",{type:'error'})
-      alert(response.data.message)
+        .then(response => {
+          if (response.data.message) {
+            cookies.set('Suscripcion', response.data.content.Suscripcion, { path: '/' });
+            window.location.href = './activa';
+            alert(response.data.message);
+          } else {
+            alert('AL PARECER HAY UN PROBLEMA CON LA ACTUALIZACION DE LA SUSCRIPCION');
+            window.location.href = './noentra';
+          }
+        });
+    } else {
+      toast('pago fallido existe algun problema', { type: 'error' });
+      alert(response.data.message);
     }
-
   };
 
   return (
     <div className='general-suscripcion'>
       <div className='container'>
-      <br></br>
-        <h1 className="titulo-Pago" style={{textAlign: "center"}}>PAGO EXAMEN ULM</h1>
         <br></br>
-        <h2 className='letraArriba' style={{textAlign: "center"}}>BANCO DE PREGUNTAS ULM</h2>
-        <h3 className='letraAbajo' style={{textAlign: "center"}}>PRECIO DEL PRODUCTO: {product.price} EUROS</h3>
+        <h1 className='titulo-Pago' style={{ textAlign: 'center' }}>
+          PAGO EXAMEN ULM
+        </h1>
+        <br></br>
+        <h2 className='letraArriba' style={{ textAlign: 'center' }}>
+          BANCO DE PREGUNTAS ULM
+        </h2>
+        <h3 className='letraAbajo' style={{ textAlign: 'center' }}>
+          PRECIO DEL PRODUCTO: {product.price} EUROS
+        </h3>
         <br></br>
         <div className='formL'>
           <StripeCheckout
-            className="center"
+            className='center'
             token={handleToken}
-            stripeKey="pk_test_51Ns7BdFxlOv4gV2Cr4mU5j5ALd479RPRjBS1MJOc4ixxcoj9VolIlL4Y06N1nUFh1kf09bRpkBXGHM5ZfMvpGtqs00r3YryaYs"
+            stripeKey={stripePublicKey}
             description={product.description}
             amount={product.price * 100}
             name={product.name}
             billingAddress
-            shippingAddress
-          >
+            shippingAddress>
             <button className='center'>PAGAR</button>
           </StripeCheckout>
         </div>
